37a25f4a901bf9a86430a42b63e030af95001d59
Nahom Mersha Alehegne <nahom.mersha.alehegne@gmail.com>
2026-05-19 22:17:16 +0200

    Initial chatbot setup

---
+++ backend/app.py
@@ -0,0 +1,3 @@
+# This will be our main entry point of our Flask server guys!
+# Always communicate before changing anything here!
+# And also in similar senetive files!
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Doctor Appointment Chatbot</h1>
+    </div>
+  )
+}
+
+export default App
