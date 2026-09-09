aead4d63e5c4aa62f00846a39900ce8bf1596541
Nahom Mersha Alehegne <nahom.mersha.alehegne@gmail.com>
2026-09-09 21:32:31 +0200

    Configure Azure startup server

--- backend/app.py
+++ backend/app.py
@@ -81,6 +81,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", 8000)),
         debug=False,
+        allow_unsafe_werkzeug=True,
     )
